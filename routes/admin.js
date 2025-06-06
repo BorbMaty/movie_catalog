cd673b213035b1d73c9764eb9b29c8805a865d06
LucyFair <borb.maty@yahoo.com>
2025-06-06 16:09:40 +0300

    added new colors, stylized the admin panel, and solved a bug

--- routes/admin.js
+++ routes/admin.js
@@ -5,17 +5,19 @@ const { requireLogin, requireAdmin } = require('../middleware/auth');
 // ADMIN PANEL MEGJELENÍTÉSE
 router.get('/admin', requireLogin, requireAdmin, (req, res) => {
   req.db.query(
-    `SELECT r.id AS review_id, r.text, r.user_id, r.movie_id,
-            u.name, m.title_hu
+    `SELECT r.id AS review_id, r.text, r.created_at, u.name, m.title_hu
      FROM reviews r
      JOIN users u ON r.user_id = u.id
      JOIN movies m ON r.movie_id = m.id`,
     (err, reviews) => {
       if (err) return res.send('Hiba');
-      res.render('admin', { reviews });
+
+      res.render('admin', { reviews }); 
     }
   );
 });
+
+
 
 // KRITIKA TÖRLÉSE
 router.post('/admin/delete-review/:id', requireLogin, requireAdmin, (req, res) => {
